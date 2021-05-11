2223088eb4534966b49a2c746a1fc81e718e5293
renenae <79869201+renenae@users.noreply.github.com>
2021-05-11 10:53:37 -0600

    Add files via upload

--- tkinterTest.py
+++ tkinterTest.py
@@ -3,10 +3,7 @@ from tkinter import *
 top = Tk()
 playlist = []
 
-def results():
-    result = E1.get()
-    playlist.append(result)
-    E1.delete(0, END)
+
 
 def printList():
     print(playlist)
@@ -26,35 +23,45 @@ def mainMenu():
     clearWindow()
     LMain = Label(top, text = "Block 5 GUI Projects")
     LMain.grid(column = 0, row = 1)
-    B1Main = Button(text= "Week 1", bg= "white")
+    B1Main = Button(text= "Week 1", bg= "white", command = week1)
     B1.grid(column= 0, row = 2)
     B2Main = Button(text = "Week 2", bg= "white")
     B2.grid(column= 0, row = 3)
     B3Main = Button(text = "Week 3", bg= "white")
     B3.grid(column= 0, row = 4)
 
-# This is a label widget
-L1 = Label(top, text="Playlist Generator")
-L1.grid(column= 0, row= 1)
+def week1():
+    clearwindow()
+    def results():
+        result = E1.get()
+        playlist.append(result)
+        E1.delete(0, END)
+        
+    # This is a label widget
+    L1 = Label(top, text="Playlist Generator")
+    L1.grid(column= 0, row= 1)
 
-# This is an Entry widget
-E1 = Entry(top, bd = 5)
-E1.grid(column= 0, row= 2)
+    # This is an Entry widget
+    E1 = Entry(top, bd = 5)
+    E1.grid(column= 0, row= 2)
 
-# This is a Button widget 
-B1 = Button(text= "  +  ", bg="yellow", command= results)
-B1.grid(column= 1, row= 2)
+    # This is a Button widget 
+    B1 = Button(text= "  +  ", bg="yellow", command= results)
+    B1.grid(column= 1, row= 2)
 
-B2 = Button(text= "  =  ", bg="cyan", command= printList)
+    B2 = Button(text= "  =  ", bg="cyan", command= printList)
+    B2.grid(column= 2, row= 2)
 
+    B3 = Button(text = "Export List", bg = "yellow", command= exportList)
+    B3.grid(column= 0, row= 3)
 
-B2.grid(column= 2, row= 2)
-
-B3 = Button(text = "Export List", bg = "yellow", command= exportList)
-B3.grid(column= 0, row= 3)
-
-Bexit = Button(text = "Clear Window", bg ="red", command= clearWindow)
-Bexit.grid(column= 1, row= 3)
+    Bexit = Button(text = "Clear Window", bg ="red", command= mainMenu)
+    Bexit.grid(column= 1, row= 3)
 
 
+if __name__=="__main__":
+    mainMenu()
+    top.mainloop()
+
+    
 top.mainloop()
