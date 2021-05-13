d85d3a4e1dbac7609468002a49fe8179b665e24c
renenae <79869201+renenae@users.noreply.github.com>
2021-05-13 11:00:35 -0600

    Add files via upload

--- tkinterTest.py
+++ tkinterTest.py
@@ -1,8 +1,11 @@
 from tkinter import *
+import random
 
 top = Tk()
 playlist = []
-
+myRolls = []
+dieType = 0
+rollTimes = 0
 
 
 def printList():
@@ -25,13 +28,13 @@ def mainMenu():
     LMain.grid(column = 0, row = 1)
     B1Main = Button(text= "Week 1", bg= "white", command = week1)
     B1Main.grid(column= 0, row = 2)
-    B2Main = Button(text = "Week 2", bg= "white")
+    B2Main = Button(text = "Week 2", bg= "white", command = week2)
     B2Main.grid(column= 0, row = 3)
     B3Main = Button(text = "Week 3", bg= "white")
     B3Main.grid(column= 0, row = 4)
 
 def week1():
-    clearwindow()
+    clearWindow()
     def results():
         result = E1.get()
         playlist.append(result)
@@ -52,21 +55,48 @@ def week1():
     B2 = Button(text= "  =  ", bg="cyan", command= printList)
     B2.grid(column= 2, row= 2)
 
-    B3 = Button(text = "Export List", bg = "yellow", command= exportList)
+    B3 = Button(text = "Export List", bg = "yellow", command = exportList)
     B3.grid(column= 0, row= 3)
 
-    Bexit = Button(text = "Clear Window", bg ="red", command= mainMenu)
+    Bexit = Button(text = "Main Menu", bg ="red", command = mainMenu)
     Bexit.grid(column= 1, row= 3)
 
 def week2():
-    clearWindow()
-    B1Week2 = Button()
-    L1Week2 = Label()
-    L2Week2 = Label()
-    L3Week2 = Label()
-    E1Week2 = Entry()
-    E2Week2 = Entry()
+    def rollDice():
+    #update variable data
+        dieType = E2Week2.get()
+        rollTimes = E1Week2.get()
+        #clear the window AFTER updating variables
+        clearWindow()
+        #roll the dice
+        for x in range(0, int(rollTimes)):
+            myRolls.append(random.randint(1, int(dieType)))
 
+        #build results window 
+        L4Week2 = Label()
+        L5Week2 = Label()
+        B2Week2 = Button()
+
+        
+    
+    clearWindow()
+    B1Week2 = Button(text= "Roll Em!", bg= "yellow")
+    B1Week2.grid(column= 2, row= 4)
+
+    L1Week2 = Label(top, text= "Dice roller app")
+    L1Week2.grid(column= 2, row= 1)
+    
+    L2Week2 = Label(top, text= "# of Rolls")
+    L2Week2.grid(column= 0, row= 2)
+    
+    L3Week2 = Label(top, text= "# of Sides")
+    L3Week2.grid(column= 3, row= 2)
+    
+    E1Week2 = Entry(top, bd = 5)
+    E1Week2.grid(column= 0, row= 3)
+    
+    E2Week2 = Entry(top, bd = 5)
+    E2Week2.grid(column= 3, row= 3)
 
 
 
